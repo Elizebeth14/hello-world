c3acd2015235f9f08511a4660f7dd3456bc58207
Elizebeth14 <elizebeth14@gmail.com>
2018-01-24 11:09:11 +0900

    micropost_page created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180123023358) do
+ActiveRecord::Schema.define(version: 20180124020044) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
